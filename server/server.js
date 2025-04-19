bd7436007ef2e3e394076f3c0634a895f046d7c2
Rishav <rishavkumar25072@gmail.com>
2025-04-19 15:39:27 +0530

    updated env variables

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const http = require('http');
 const SocketService = require('./services/SocketService');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
